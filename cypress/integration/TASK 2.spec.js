26808a2ef697b5c33176e77290afcd672c5c8bc6
Ieremia Loredana <ieremia.loredana@yahoo.com>
2021-08-18 14:58:55 +0300

    task 1

--- cypress/integration/TASK 2.spec.js
+++ cypress/integration/TASK 2.spec.js
@@ -18,6 +18,7 @@ describe('Check purchase functionality', () => {
         cy.get('body > div > div > div.main-container.col1-layout > div > div > div > ul > li > ul > li > span').should('have.text','IPhone was added to your shopping cart.')  
         cy.get('.method-checkout-cart-methods-onepage-bottom > .button').click()  
         cy.get('#onepage-guest-register-button').click()
+        cy.get('#onepage-guest-register-button').should('have.text','Continue')
         
     
     })
@@ -44,12 +45,32 @@ describe('Check purchase functionality', () => {
         cy.get('#payment-buttons-container > .button').click()
         cy.get('#review-buttons-container > .button').click()
         cy.get('body > div > div > div.main-container.col1-layout > div > div > div.page-title > h1').should('have.text','Your order has been received.')
-        cy.get('body > div > div > div.main-container.col1-layout > div > div > h2').should('have.text','Thank you for your purchase!')
-        
-        
-        
-
+        cy.get('body > div > div > div.main-container.col1-layout > div > div > p:nth-child(3)').should('exist')
     })
-    
-})
-
+    it.only('check the order status', () => {
+        cy.get('#search').type('iphone')
+        cy.get('.input-box > .button').click()
+        cy.get('.product-info > .actions > .button').click()
+        cy.get('body > div > div > div.main-container.col1-layout > div > div > div > ul > li > ul > li > span').should('have.text','IPhone was added to your shopping cart.')  
+        cy.get('.method-checkout-cart-methods-onepage-bottom > .button').click()  
+        cy.get('#onepage-guest-register-button').click() 
+        cy.get('#billing\\:firstname').type('Mike')
+        cy.get('#billing\\:middlename').type('Samuel')
+        cy.get('#billing\\:lastname').type('Smith')
+        cy.get('#billing\\:email').type('mike.smith@gmail.com')
+        cy.get('#billing\\:street1').type('5th avenue,California')
+        cy.get('#billing\\:city').type('California')
+        cy.get('#billing\\:postcode').type('24430')
+        cy.get('#billing\\:country_id').select('Serbia')
+        cy.get('#billing\\:telephone').type('0654345678')
+        cy.get('#co-billing-form > :nth-child(1) > .form-list > :nth-child(2) > label').click()
+        cy.get('#billing-buttons-container > .button').click()
+        cy.get('#shipping-method-buttons-container > .button').click()
+        cy.get('#dt_method_checkmo > label').click()
+        cy.get('#payment-buttons-container > .button').click()
+        cy.get('#review-buttons-container > .button').click()
+        cy.get('body > div > div > div.main-container.col1-layout > div > div > div.page-title > h1').should('have.text','Your order has been received.')
+        cy.get('document.querySelector("body > div > div > div.main-container.col1-layout > div > div > p:nth-child(3)")').should(setTimeout(function() { 
+        
+    })
+}
--- cypress/integration/TASK 3.spec.js
+++ cypress/integration/TASK 3.spec.js
@@ -1,6 +1,40 @@
-// TASK.spec.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
+describe('Task 3', () => {
+    beforeEach(() => {
+  
+      cy.visit('http://live.demoguru99.com')
+  
+    })
+  
+    it('cypress test for updating account information', () => {
+        cy.get('.skip-account').click()
+        cy.get('#header-account > .links > ul > .last > a').click()
+        cy.get('#email').type('mike.smith@gmail.com')
+        cy.get('#pass').type('12345678')
+        cy.get('#send2').click()
+        cy.get('.skip-account').click()
+        cy.get('#header-account > .links > ul > .first > a').click()
+        cy.get(':nth-child(3) > .col2-set > .col-1 > .box > .box-title > a').click()
+        cy.get('#firstname').type('Mikael')
+        cy.get('#middlename').type('Sam')
+        cy.get('#current_password').type('12345678')
+        cy.get('.buttons-set > .button').click()
+        cy.get('body > div > div > div.main-container.col2-left-layout > div > div.col-main > div > div > ul > li > ul > li > span').should('have.text','The account information has been saved.')
+
+    })
+    it.only('Check the Add new address functionality', () => {
+        cy.get('.skip-account').click()
+        cy.get('#header-account > .links > ul > .last > a').click()
+        cy.get('#email').type('mike.smith@gmail.com')
+        cy.get('#pass').type('12345678')
+        cy.get('#send2').click()
+        cy.get('.current').click()
+        cy.get('#street_1').select('5th avenue,California')
+        cy.get('#city').type('California')
+        cy.get('#region').type('California')
+        cy.get('#zip').type('90012')
+        cy.get('#country').select('United States')
+        cy.get('.buttons-set > .button').click()
+    })
+})    
+
+
